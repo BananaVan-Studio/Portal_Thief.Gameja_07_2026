6c34d386b5436903c6479b4f508adcb5aff19680
Adrián <adrian.lin.xiang@gmail.com>
2026-09-09 11:54:32 +0200

    Update order.gd

--- thief/order.gd
+++ thief/order.gd
@@ -1,3 +1,4 @@
+@tool
 class_name Order
 extends Marker2D
 
@@ -8,8 +9,41 @@ enum Command {
 	ROTATE,
 }
 
-@export var index: int = -1
-@export var action: Command = Command.NONE
-@export var execution_time: float = -1
-@export var time_until_next_order: float = -1
-@export var rotation_deg: float = -1
+@export var index: int = -1:
+	set(value):
+		index = value
+		update_configuration_warnings()
+@export var action: Command = Command.NONE:
+	set(value):
+		action = value
+		update_configuration_warnings()
+@export var execution_time: float = -1:
+	set(value):
+		execution_time = value
+		update_configuration_warnings()
+@export var rotation_deg: float = -1:
+	set(value):
+		rotation_deg = value
+		update_configuration_warnings()
+@export var previous_order: Order = null:
+	set(value):
+		previous_order = value
+		update_configuration_warnings()
+
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings = []
+	if action == Command.NONE or execution_time == -1 or index == -1:
+		warnings.append("Order not initialized. Some core data is missing.")
+
+	if action == Command.ROTATE and rotation_deg == -1:
+		warnings.append("Received rotation order, but rotation_deg was not introduced.")
+
+	if action == Command.STAY:
+		if previous_order == null:
+			return warnings 
+
+		if previous_order.global_position != global_position:
+			warnings.append("Received stay order, but marker is not in the same position as previous order.")
+
+	return warnings
